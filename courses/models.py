245e0e77e00bc67646c4a634e1423213681ed59a
kimiajam <kimiajaam@gmail.com>
2026-09-20 22:28:56 +0330

    Initial LMS project

---
+++ courses/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ courses/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Course, Lesson
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'courses/course_list.html', {'courses': courses})
+
+
+def course_detail(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    lessons = course.lesson_set.all()
+
+    return render(
+        request,
+        'courses/course_detail.html',
+        {'course': course, 'lessons': lessons}
+    )
+
+
+def lesson_detail(request, lesson_id):
+    lesson = get_object_or_404(Lesson, id=lesson_id)
+
+    return render(
+        request,
+        'courses/lesson_detail.html',
+        {'lesson': lesson}
+    )
